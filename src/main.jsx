d9c059c11bf2530c2d67c5e1db69682f832d58aa
Nalenar <nichrom.isme@gmail.com>
2023-04-03 19:54:57 +0300

    Added Welcome page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Route path="/" element={<Welcome />} />
+    </Router>
+  </React.StrictMode>
+);
---
+++ src/routes/Welcome.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+
+const Welcome = () => {
+  return (
+    <main className="flex h-screen w-screen flex-row items-center justify-center">
+      {/* leftImage */}
+      <div className="hidden">
+        <img src="src/assets/welcome-page/welcome-left.png" alt="happy man" />
+      </div>
+
+      {/* hero */}
+      <div className="flex h-full flex-col items-center justify-center">
+        <img
+          src="src/assets/welcome-page/logo.png"
+          alt="logo"
+          className="w-24"
+        />
+        <div className="my-2 w-72 text-center font-roboto">
+          <h1 className="mb-5 text-5xl">To Do</h1>
+          <img
+            className="max-w- lg:hidden"
+            src="src/assets/welcome-page/welcome-center.png"
+            alt="happy women and men"
+          />
+          <p className="my-3 text-center text-3xl">
+            Это приложение поможет вам организовать повседневные задачи
+          </p>
+        </div>
+
+        <div className="flex flex-col">
+          <button className="mb-2 rounded-3xl border-2 border-blue-500 bg-blue-500 px-4 py-2 text-xl text-white shadow-md shadow-blue-600">
+            <Link to={"/app"}>Начало работы</Link>
+          </button>
+          <button className="rounded-3xl border-2 border-gray-500 bg-gray-500 px-4 py-2 text-xl text-white shadow-md shadow-gray-600">
+            <Link to={"/about"}>Подробнее</Link>
+          </button>
+        </div>
+      </div>
+
+      {/* rightImage */}
+      <div className="hidden">
+        <img
+          src="src/assets/welcome-page/welcome-right.png"
+          alt="happy women"
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Welcome;
