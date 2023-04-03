9862db7ef4ac435880eeb78df85bc424305ef1ff
Nalenar <nichrom.isme@gmail.com>
2023-04-03 19:59:05 +0300

    Bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
+
+import { Welcome } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <Route path="/" element={<Welcome />} />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
