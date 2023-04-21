ab860a6d832cab2ddd39e986153cfea748c5d296
Nalenar <nichrom.isme@gmail.com>
2023-04-21 13:49:05 +0300

    Added toggle complete & delete todo features

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,64 +1,26 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 
 const Day = () => {
-  const [todos, setTodos] = useState(() => {
-    const savedTodos = localStorage.getItem("todos");
-    return savedTodos ? JSON.parse(savedTodos) : [];
-  });
-  const [todo, setTodo] = useState("");
+  const todos = useSelector(state => state.todos)
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") return;
-
-    const newTodo = {
-      id: todos.length + 1,
-      text: todo.trim(),
-      complete: false,
-    };
-
-    setTodos([newTodo, ...todos]);
-    setTodo("");
-  };
-
-  const handleDeleteClick = (id) => {
-    const removeItem = todos.filter((el) => el.id !== id);
-    setTodos(removeItem);
-  };
-
-  const handleComplete = (id) => {
-    const updatedTodos = todos.map((el) => {
-      if (el.id === id) {
-        el.complete = !el.complete;
-      }
-      return el;
-    });
-
-    setTodos(updatedTodos);
-  };
 
   return (
     <div className="overflow-y-auto">
       <h2 className="text-3xl my-5 font-roboto text-center text-blue-700">
         Мой день
       </h2>
-      <TodoInput
-        todo={todo}
-        onFormSubmit={handleFormSubmit}
-        onInputChange={handleInputChange}
-      />
-      {todos.map((el) => (
+      <TodoInput />
+      {todos.map((todo) => (
         <TodoItem
-          key={el.id}
-          todo={el}
-          onDeleteClick={handleDeleteClick}
-          handleComplete={handleComplete}
+          key={todo.id}
+          todo={todo}
         />
       ))}
     </div>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,28 +1,39 @@
 import { Icon } from "@iconify/react";
+import { useDispatch } from "react-redux";
+import { toggleComplete, deleteTodo } from "../redux/todoSlice";
 
-const TodoItem = (props) => (
-  <div className="mx-3 mt-3 h-12 flex items-center shadow-md border border-gray-200 rounded last-of-type:mb-2">
-    <div className="mx-3" onClick={() => props.handleComplete(props.todo.id)}>
-      {props.todo.complete ? (
-        <Icon icon="ion:checkmark-circle" width="27" color="#3b82f6" />
-      ) : (
-        <Icon icon="mdi:circle-outline" width="27" color="#3b82f6" />
-      )}
+const TodoItem = (props) => {
+  const dispatch = useDispatch();
+
+  const handleCompleteClick = () => {
+    dispatch(toggleComplete({ id: props.todo.id, completed: !props.todo.completed}))
+  };
+
+  const handleDeleteClick = () => {
+    dispatch(deleteTodo({ id: props.todo.id }))
+  }
+
+  return (
+    <div className="mx-3 mt-3 h-12 flex items-center shadow-md border border-gray-200 rounded last-of-type:mb-2">
+      <div className="mx-3 cursor-pointer" onClick={handleCompleteClick}>
+        {props.todo.completed ? (
+          <Icon icon="ion:checkmark-circle" width="27" color="#3b82f6" />
+        ) : (
+          <Icon icon="mdi:circle-outline" width="27" color="#3b82f6" />
+        )}
+      </div>
+      <p
+        className={`h-full w-full flex items-center overflow-x-auto text ${
+          props.todo.completed ? "line-through text-gray-400 italic" : ""
+        }`}
+      >
+        {props.todo.title}
+      </p>
+      <div onClick={handleDeleteClick} className="h-[30px] w-[30px] flex justify-center items-center cursor-pointer mx-3">
+        <Icon icon="ion:trash" width={27} color="#ef4444" />
+      </div>
     </div>
-    <p
-      className={`h-full w-full flex items-center overflow-x-auto text ${
-        props.todo.complete ? "line-through text-gray-400 italic" : ""
-      }`}
-    >
-      {props.todo.text}
-    </p>
-    <div
-      className="h-[30px] w-[30px] flex justify-center items-center cursor-pointer mx-3"
-      onClick={() => props.onDeleteClick(props.todo.id)}
-    >
-      <Icon icon="ion:trash" width={27} color="#ef4444" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default TodoItem;
