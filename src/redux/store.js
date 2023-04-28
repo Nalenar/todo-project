5d9bbceec623cece465d7e5f113d2ecc6677700d
Nalenar <nichrom.isme@gmail.com>
2023-04-28 19:28:11 +0300

    store in docs

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,18 @@
+/**
+ * Главный файл который использует store
+ * из библиотеки @reduxjs/toolkit
+ * @module
+ * 
+ * Экспортирует объект хранящий в себе редюсеры слайсов
+ * @exports 
+ * 
+ * @param {object} reducer - Содержит в себе название состояния и относящийся к нему редюсер
+ * 
+ * @param {object} todos - Содержит в себе все экшены, редюсеры и состояния для задач
+ * @param {object} lists - Содержит в себе все экшены, редюсеры и состояния для списков 
+ * @param {object} common - Содержит в себе все экшены, редюсеры и состояния для стандартных функций (например стилей, окон и т.д.)
+ */
+
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
 import commonReducer from "./commonSlice";
