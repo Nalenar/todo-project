efb7ecc560f55f3e7a1ec36dbee0b2cc41151aa3
Nalenar <nichrom.isme@gmail.com>
2023-04-28 18:48:54 +0300

    Added simple searchbar in header

---
+++ src/components/header/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <>
+        <input type="text" placeholder='Поиск...' className='form-input relative lg:w-96 lg:left-20 rounded-lg mx-auto placeholder:text-gray-300' />
+    </>
+  )
+}
+
+export default SearchBar
