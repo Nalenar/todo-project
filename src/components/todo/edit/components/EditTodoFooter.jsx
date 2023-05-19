e72bfad68e6e2069ae1bae775b7f238f3f3dfa33
Nalenar <nichrom.isme@gmail.com>
2023-05-19 04:12:02 +0300

    Added tests for EditTodoFooter component

--- src/components/todo/edit/components/EditTodoFooter.jsx
+++ src/components/todo/edit/components/EditTodoFooter.jsx
@@ -18,11 +18,11 @@ const EditTodoFooter = ({ todo }) => {
 
   return (
     <div className="flex h-[10%] items-center justify-between border-t text-gray-500">
-      <button onClick={handleEditClose}>
+      <button onClick={handleEditClose} role="button-close_edit">
         <Icon icon="line-md:menu-fold-right" className="text-3xl" />
       </button>
       <p className="text-lg">Создано сегодня</p>
-      <button onClick={handleDeleteClick}>
+      <button onClick={handleDeleteClick} role="button-delete_todo">
         <Icon icon="ion:trash" className="text-3xl" />
       </button>
     </div>
